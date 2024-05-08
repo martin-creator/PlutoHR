36b2f4dbec7a1435ac438e314ad712a6b84323c7
martin-creator <martinlubowa@outlook.com>
2024-05-08 23:25:27 +0300

    update urls.py

--- backend/plutohr_api/plutohr_api/urls.py
+++ backend/plutohr_api/plutohr_api/urls.py
@@ -15,8 +15,39 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
+    path('api/auth/', include('authentication.urls')),
+
+
+
+
 ]
+
+
+# from django.urls import include, path
+# from rest_framework import routers
+
+# from authentication import views
+
+# router = routers.DefaultRouter()
+# # router.register(r'users', views.UserViewSet)
+# # router.register(r'groups', views.GroupViewSet)
+
+
+
+# # Wire up our API using automatic URL routing.
+# # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
+
+# include('rest_framework.urls') helps to include the login and logout URLs for the browsable API.
+
