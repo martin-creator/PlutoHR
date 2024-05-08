a6574c169aa55e6c74ac6fa9ea0d873ea87f22de
martin-creator <martinlubowa@outlook.com>
2024-05-08 23:24:48 +0300

    update models.py

--- backend/plutohr_api/authentication/models.py
+++ backend/plutohr_api/authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('Manager', 'Manager'),
+        ('Employee', 'Employee'),
+    )
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
