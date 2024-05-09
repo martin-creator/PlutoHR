852f1675eb7868b6b1f998b963764ed0ed37e140
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-05-09 08:55:56 +0300

    Added homepage items

--- frontend/src/Component/Home.js
+++ frontend/src/Component/Home.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { FaHome } from 'react-icons/fa'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h3 className='home-heading'>
+        <span><FaHome /></span>
+        Home
+      </h3>
+      <div className='home-details'>
+        <div className='item1'>
+          <div>10</div>
+          <div>Employees</div>
+        </div>
+        <div className='item2'>
+          <div>10</div>
+          <div>Employees</div>
+        </div>
+        <div className='item3'>
+          <div>10</div>
+          <div>Employees</div>
+        </div>
+        <div className='item4'>
+          <div>10</div>
+          <div>Employees</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
