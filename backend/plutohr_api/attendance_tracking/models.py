a3c069bacb0eb31a0575128f4d0b7aa19a1617d8
martin-creator <martinlubowa@outlook.com>
2024-05-21 14:50:54 +0300

    Created attendance model

--- backend/plutohr_api/attendance_tracking/models.py
+++ backend/plutohr_api/attendance_tracking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from employee.models import Employee
 
 # Create your models here.
+
+class Attendance(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField()
+    time_in = models.TimeField()
+    time_out = models.TimeField()
+    def __str__(self):
+        return self.employee.username + ' ' + str(self.date)
+    
