6576fe6f8216da9c3a4e1361d1a10ee994d8b0d8
martin-creator <martinlubowa@outlook.com>
2024-05-22 23:10:56 +0300

    update models.py

--- backend/plutohr_api/employee/models.py
+++ backend/plutohr_api/employee/models.py
@@ -5,7 +5,8 @@ from manager.models import Manager
 
 class Employee(User):
     employee_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
-    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, blank=True, null=True)
+    managers = models.ForeignKey(Manager, on_delete=models.CASCADE, blank=True, null=True)
+    leave_balance = models.IntegerField(default=10, blank=True)
 
     # The manager is a foreign key to the Manager model. This means that the manager field in the Employee model will contain a reference to a Manager object. The on_delete=models.CASCADE argument specifies that if a Manager object is deleted, all the Employee objects that reference that Manager object will also be deleted.
     # It is a one to many relationship between the Manager and Employee models. A manager can have many employees, but an employee can only have one manager.
