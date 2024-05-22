b5e873a67a7448e78e39f2062a70c07452f9124e
martin-creator <martinlubowa@outlook.com>
2024-05-22 23:11:01 +0300

    update urls.py

--- backend/plutohr_api/employee/urls.py
+++ backend/plutohr_api/employee/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from employee.views import EmployeeRegistrationView, EmployeeListView, EmployeeDetailView
 from leave_management.views import LeaveManagementView, LeaveDetailView
+from attendance_tracking.views import AttendanceRecordView
 
 urlpatterns = [
     path('register/', EmployeeRegistrationView.as_view(), name='employee-registration'),
@@ -9,5 +10,7 @@ urlpatterns = [
 
     path('leave/', LeaveManagementView.as_view(), name='leave-management'),
     path('leave/<int:pk>/', LeaveDetailView.as_view(), name='leave-detail'),
+
+    path('attendance/', AttendanceRecordView.as_view(), name='attendance-record'),
     # Add other URLs here
 ]
