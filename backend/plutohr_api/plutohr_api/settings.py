b66bbb70165a94687c5d1c7c6e053e48c17fbbcc
martin-creator <martinlubowa@outlook.com>
2024-05-31 15:44:25 +0300

    Added static root

--- backend/plutohr_api/plutohr_api/settings.py
+++ backend/plutohr_api/plutohr_api/settings.py
@@ -157,6 +157,7 @@ USE_TZ = True
 
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
